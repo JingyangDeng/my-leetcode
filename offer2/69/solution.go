1dccdf111d0d7381c6ae95d7b6ae6a401a51b69e
Jingyang Deng <jydeng0114@outlook.com>
2021-10-14 17:45:07 +0800

    add offer2 69

---
+++ offer2/69/solution.go
@@ -0,0 +1,13 @@
+//二分查找
+func peakIndexInMountainArray(arr []int) int {
+    left, right := 0, len(arr) - 1
+    for left < right {
+        mid := (left + right) >> 1
+        if arr[mid] < arr[mid + 1] {
+            left = mid + 1
+        } else {
+            right = mid
+        }
+    }
+    return left
+}
