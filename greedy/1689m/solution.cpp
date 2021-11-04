d60acd706da842cc6dbacd03e172db362f758399
Jingyang Deng <jydeng0114@outlook.com>
2021-11-05 00:01:53 +0800

    add 1689

---
+++ greedy/1689m/solution.cpp
@@ -0,0 +1,11 @@
+//简单遍历
+class Solution {
+public:
+    int minPartitions(string n) {
+        int ret = 0;
+        for (auto c : n) {
+            ret = max(ret, c - '0');
+        }
+        return ret;
+    }
+};
