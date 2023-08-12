a4c522fd27a0f77988caf7939b2c4c265fb4f4b6
Jingyang Deng <jydeng0114@outlook.com>
2023-08-13 00:35:16 +0800

    add 88

---
+++ 1-100/88/solution.cpp
@@ -0,0 +1,21 @@
+// 从后往前逐一确定，O(m+n)
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int p = m + n - 1;
+        int p1 = m - 1;
+        int p2 = n - 1;
+        while(p >= 0) {
+            if(p1 < 0) {
+                nums1[p--] = nums2[p2--];
+            } else if(p2 < 0) {
+                nums1[p--] = nums1[p1--];
+            } else if(nums1[p1] < nums2[p2]) {
+                nums1[p--] = nums2[p2--];
+            } else {
+                nums1[p--] = nums1[p1--];
+            }
+        }
+        return;
+    }
+};
